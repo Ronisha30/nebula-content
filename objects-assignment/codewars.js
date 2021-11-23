2fbf9987139416b4a3684fb835b75519a3fc0272
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-22 22:45:37 -0600

    Objects

---
+++ objects-assignment/codewars.js
@@ -0,0 +1,5 @@
+function greeting (name){
+    if(name === "Johnny")
+      return "Hello, my love!";
+    return "Hello, " + name + "!";
+  }
---
+++ objects-assignment/objects-assignment.js
@@ -0,0 +1,73 @@
+//  // 1.   Create an object for your dream vehicle Give it a minimum of 6 key-value pair //
+//  // This object should contain many datatypes (string, number, boolean, null, array)
+//  const vehicle = {  
+//    make: 'Land Range Rover' , 
+//    bodystyle: 'standard ', 
+//    model: 'Frist Edition', 
+//    maxspeed:'150 mph', 
+//    maxpower: 395,
+//    color: 'black', 
+//    engine: 'P530 AWD Automatic', 
+//    year: 2021, 
+//    cost: 140000000,
+//   engineOn: true
+
+//  }
+//  console.log(vehicle);
+
+
+//  //2. Using dot-notation Log to the console 3 properties from the previous object
+// vehicle.color ='black', vehicle.maxpower = '395', vehicle.model = 'Frist Edition', 
+// console.log(vehicle);
+
+
+//  // 3. Using bracket-notation Log to the console 3 properties from the previous object
+// vehicle['year'] = '2021',vehicle['make'] = 'Land Range Rover', vehicle['engine'] = 'P530 AWD Automatic',
+// console.log(vehicle);
+
+
+// // 4. Try and log a property that doesn't already exist - what output do we get?
+//  console.log(vehicle.size);
+    
+    
+
+
+// // 5. Add a new key-value pair to the vehicle.
+// vehicle.height = '3.33',
+//  console.log(vehicle); 
+
+
+// // 6. Using bracket-notation update a property on the vehicle.
+// vehicle['color'] = 'blue';
+//  console.log(vehicle)
+
+//  //  7. Using dot-notation update a property on the vehicle.
+//  vehicle.engine = 'V8';
+//  console.log(vehicle)
+
+// // 8. Create a method for turning your vehicle on
+// function startVehicle(){
+//   vehicle.engineOn = true;
+// }
+// vehicle.startVehicle = startVehicle
+
+// // 9. Create a method for turning your vehicle off
+// function stopVehicle(){
+//   vehicle.engineOn = false;
+// }
+// vehicle.stopVehicle = stopVehicle
+
+
+//10.
+//a. Check if your vehicle is on (it should be off)
+e
+  
+// b. Start your vehicle
+
+// c. Check if your vehicle is on (it should be on)
+
+// d. Stop your vehicle
+
+// e. Check if your vehicle is on (it should be on)
+
+  
